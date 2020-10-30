e0eeaa6388bdf8386518397510b7820b55019a1c
sairajsawant <sairaj.sawant@phonepe.com>
2020-10-30 10:46:42 +0530

    --refactor: arrow fun

--- background.js
+++ background.js
@@ -5,13 +5,13 @@ function findTabAndSetActive() {
 	browser.tabs.query({}, (tabs) => {
 		tabs.forEach((tab) => {
 			if(tab.url.startsWith(googleMeet,0)){
-	  			console.log(tab.url)
-	  			browser.windows.update(tab.windowId,{focused:true}, function(window) {});
- 				browser.tabs.update(tab.id, {highlighted:true,active:true},function(tab){
- 					console.log(tab.url + " is active");
- 				});
- 				return false;
-	  		}
+				console.log(tab.url)
+				browser.windows.update(tab.windowId,{focused:true},() => {});
+				browser.tabs.update(tab.id, { highlighted:true,active:true },(tab) => {
+					console.log(tab.url + " is active");
+				});
+				return false;
+			}
 		})
 	});
 };
