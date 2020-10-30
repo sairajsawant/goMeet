736e71e784cb4b6de4fb545ac078ab89d56b115f
sairajsawant <sairaj.sawant@phonepe.com>
2020-10-30 09:18:09 +0530

    --update: tested firefox

--- background.js
+++ background.js
@@ -1,14 +1,14 @@
-chrome.browserAction.onClicked.addListener(function(tab) { findTabAndSetActive() });
-
+var browser = browser || chrome
+browser.browserAction.onClicked.addListener(function(tab) { findTabAndSetActive() });
 function findTabAndSetActive() {
 	const googleMeet = "https://meet.google.com";
-	chrome.tabs.query({}, function(tabs) {
+	browser.tabs.query({}, function(tabs) {
 		console.log(tabs)
 		tabs.forEach((tab) => {
 			if(tab.url.startsWith(googleMeet,0)){
 	  			console.log(tab.url)
-	  			chrome.windows.update(tab.windowId,{focused:true}, function(window) {});
- 				chrome.tabs.update(tab.id, {highlighted:true,active:true},function(tab){
+	  			browser.windows.update(tab.windowId,{focused:true}, function(window) {});
+ 				browser.tabs.update(tab.id, {highlighted:true,active:true},function(tab){
  					console.log(tab.url + " is active")
  				});
  				return false;
