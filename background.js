9ab84f957d9ddc96c2f406aea03a606700f5da05
sairajsawant <sairaj.sawant@phonepe.com>
2020-10-30 11:43:31 +0530

    --refactor: use of url wildcard

--- background.js
+++ background.js
@@ -1,17 +1,15 @@
 var browser = browser || chrome
 browser.browserAction.onClicked.addListener(() => { findTabAndSetActive() });
 function findTabAndSetActive() {
-	const googleMeet = "https://meet.google.com";
-	browser.tabs.query({}, (tabs) => {
+	const googleMeet = "*://meet.google.com/*";
+	browser.tabs.query({url: googleMeet}, (tabs) => {
 		tabs.forEach((tab) => {
-			if(tab.url.startsWith(googleMeet,0)){
-				console.log(tab.url)
-				browser.windows.update(tab.windowId,{focused:true},() => {});
-				browser.tabs.update(tab.id, { highlighted:true,active:true },(tab) => {
+			console.log(tab.url)
+			browser.windows.update(tab.windowId,{focused:true},() => {});
+			browser.tabs.update(tab.id, { highlighted:true,active:true },(tab) => {
 					console.log(tab.url + " is active");
-				});
-				return false;
-			}
+			});
+			return false;
 		})
 	});
 };
