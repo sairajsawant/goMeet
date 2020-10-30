fafdc4f190e15646bebfc93dbdfd624dc41e2532
sairajsawant <sairaj.sawant@phonepe.com>
2020-10-30 10:43:18 +0530

    --refactor: arrow fun

--- background.js
+++ background.js
@@ -1,15 +1,14 @@
 var browser = browser || chrome
-browser.browserAction.onClicked.addListener(function(tab) { findTabAndSetActive() });
+browser.browserAction.onClicked.addListener(() => { findTabAndSetActive() });
 function findTabAndSetActive() {
 	const googleMeet = "https://meet.google.com";
-	browser.tabs.query({}, function(tabs) {
-		console.log(tabs)
+	browser.tabs.query({}, (tabs) => {
 		tabs.forEach((tab) => {
 			if(tab.url.startsWith(googleMeet,0)){
 	  			console.log(tab.url)
 	  			browser.windows.update(tab.windowId,{focused:true}, function(window) {});
  				browser.tabs.update(tab.id, {highlighted:true,active:true},function(tab){
- 					console.log(tab.url + " is active")
+ 					console.log(tab.url + " is active");
  				});
  				return false;
 	  		}
